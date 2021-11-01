ececf3ef85097310009ea734483e96f4c86c59e2
Haskell Cappers <45224182+Hcappers@users.noreply.github.com>
2021-11-01 19:37:56 -0400

    Delete 2019 CSCE1030 directory

--- 2019 CSCE1030/Lab 8/Lab8_HEADER.h
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-using namespace std;
-
-const int COLS = 5; // 4 quiz grades, 1 quiz average
-
-void computeMinimum (float grades[][COLS], int ROWS, float minimium[]);
-void computeAverage (float grades[][COLS], int ROWS, float minimium[]);
